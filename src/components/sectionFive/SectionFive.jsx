80626c8cf29009f9c0a8d55afe2aab8cf4932d89
Ahmed Aboubakr <a.m.aboubakr85@gmail.com>
2022-02-01 10:02:50 +0200

    section 5,6,7 plus subscribe and footer

---
+++ src/components/sectionFive/SectionFive.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './sectionFive.scss'
+const SectionFive = () => {
+  return (
+    <>
+      <div className='container section-five'>
+        <div className='items d-flex flex-column justify-content-center align-items-center h-75'>
+          <h3>Huge Global Network of Fast VPN</h3>
+          <p>See LaslesVPN everywhere to make it easier for you when you move locations.</p>
+          <img src='./assets/images/section-five-bg.png' alt='123' />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SectionFive
